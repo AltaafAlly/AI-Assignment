be31864277987025e68d53e0d3970e5509dd0090
AltaafAlly <altaaf313@gmail.com>
2024-04-23 21:27:25 +0200

    Part 2 sub 1 done

--- Part 2/Sub1.py
+++ Part 2/Sub1.py
@@ -1,11 +1,18 @@
 import chess
-import reconchess
+import chess.engine
+from reconchess import utilities
 
-def generate_move(fen):
-    #Initialise board with fen
-    board = che
-x = input()
-board = chess.Board(x)
+def next_moves(fen):
+    board = chess.Board(fen)
+    moves = list(board.pseudo_legal_moves)
+    moves.append(chess.Move.null())
+
+    for move in utilities.without_opponent_pieces(board).generate_castling_moves():
+        if not utilities.is_illegal_castle(board, move):
+            moves.append(move)
+
+    return sorted(set(str(move) for move in moves))
 
 
-        print(move)
+fen = input()
+print("\n".join(next_moves(fen)))
